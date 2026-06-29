565844d045015f67a6e3af630b3bed627afa13e4
Noah Pope <npope@tutanota.com>
2026-06-28 19:28:20 -0500

    adding files to new repo

---
+++ CS106A_99BottlesOfBeer.java
@@ -0,0 +1,32 @@
+/**
+ * File: CS106A_99BottlesOfBeer.java
+ * ----------------------------
+ * This Java program sings the song "99 bottles of beer on the wall"
+ * after asking the user how many bottles of beer they'd like to 
+ * start with.
+ */
+
+import acm.program.*;
+
+public class CS106A_99BottlesOfBeer extends ConsoleProgram
+{	
+	private static final int TRADITIONAL_STARTING_NUMBER_OF_BOTTLES = 99;
+	
+	public void run()
+	{ 
+		introduceApp();
+		singSong();
+	}
+	
+	
+	private void introduceApp()
+	{
+		
+	}
+	
+	
+	private void singSong()
+	{
+		
+	}
+}
